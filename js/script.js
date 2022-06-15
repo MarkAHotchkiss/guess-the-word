9321ec4f7546571fb86eb9885b1402a66951b1cd
MHotchkiss <mark.hotchkiss@ymail.com>
2022-06-15 14:22:55 -0500

    Added comments, verified entry

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Global variables
 //The unordered list where the player’s guessed letters will appear.
-const guessLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 //The button with the text “Guess!” in it.
 const buttonGuess = document.querySelector(".guess");
 //The text input where the player will guess a letter.
@@ -17,25 +17,68 @@ const messages = document.querySelector(".message");
 const buttonPlayAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 //Add Placeholders for each letter
 const placeHolder = function(word){
-    const dotSymbols = [];
+    const placeHolderDots = [];
     for (const letter of word){
         console.log(letter);
-        dotSymbols.push("●");
+        placeHolderDots.push("●");
     }
-    wordProgress.innerText = dotSymbols.join("");
+    wordProgress.innerText = placeHolderDots.join("");
 };
 
 placeHolder(word);
 
 buttonGuess.addEventListener("click", function (e) {
     e.preventDefault();
-    const guess = letterInput.value;
-    console.log(guess);
-    letterInput.value = "";  
+    messages.innerText = "";
+    const guess = letterInput.value;    
+    const inputHolder = checkInput(guess);
+
+    if (inputHolder) {
+        makeGuess(guess);
+    }
+    letterInput.value = "";
 });
+
+//function to check the validity of the input.
+const checkInput = function(input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        messages.innerText = "You need to enter a letter! There are no spaces.";
+    } else if (input.length > 1) {
+         messages.innerText = "Please enter only one letter, and try again.";
+        } else if (!input.match(acceptedLetter)){
+            messages.innerText = "Please enter a letter from A to Z. Numbers and symboles are not allowed.";
+            } else {
+                 return input;
+                }  
+};
+
+const makeGuess = function (guess){
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        messages.innerText = "You already guessed that letter. Try again.";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
+
+
+
+
+
 
 
   
+//const getData = async function () {
+//    const res = await fetch(
+//      "https://quote-garden.herokuapp.com/api/v3/quotes?author=beyonce"
+//    );
+//    const data = await res.json();
+//    console.log(data);
+// };
+// getData();
